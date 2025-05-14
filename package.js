500a3670c32070af14bf2fe2361f79fe799c21c6
EnBest2 <enbest2@gmail.com>
2025-05-14 17:37:26 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,20 @@
+{
+  "name": "flashcard-app",
+  "version": "1.0.0",
+  "description": "Web app tanuló kártyák készítéséhez fájlfeltöltési lehetőséggel.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "multer": "^1.4.5",
+    "body-parser": "^1.20.1"
+  },
+  "devDependencies": {
+    "nodemon": "^2.0.15"
+  },
+  "author": "",
+  "license": "ISC"
+}
